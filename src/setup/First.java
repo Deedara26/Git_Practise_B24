76ba2145239e8328ed42871753013818eb883fe4
Deedara26 <deeabilkarim@gmail.com>
2021-10-03 11:32:18 -0400

    Added 3rd line

--- src/setup/First.java
+++ src/setup/First.java
@@ -3,5 +3,14 @@ package setup;
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        // new line
+
+
+        // I want to do smth
+
+        // The text does matter
+
+        // third line
     }
 }
